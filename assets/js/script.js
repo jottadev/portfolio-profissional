b29170cff06ee4d5dd059e73e25208e105aa4a12
jottadev <contato.joaoythalo@gmail.com>
2023-01-11 14:52:18 -0300

    css reponsivo e nova feature/menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+//Função de copiar email
 let emailCopy = document.querySelector('#email-copy')
 let btnCopy = document.querySelector('#btn-copy')
 function copyEmail(){
@@ -5,3 +6,18 @@ function copyEmail(){
 }
 btnCopy.addEventListener('click', copyEmail)
 
+
+//Função do menu mobile
+function menuShow(){
+    let menuMobileItems = document.querySelector('.menu-mobile-items')
+    if(menuMobileItems.classList.contains('open')) {
+        menuMobileItems.classList.remove('open')
+        document.querySelector('.icon').src = "assets/imgs/menu-open.svg"
+    } else{
+        menuMobileItems.classList.add('open')
+        document.querySelector('.icon').src = "assets/imgs/close-menu.svg"
+    }
+    
+}
+let btnMenuOpen = document.querySelector('#menuOpen')
+btnMenuOpen.addEventListener('click', menuShow)
